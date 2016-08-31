ec8f615c8ca4e9d084725fead8a0ba8e47907e3a
林悦斌 <linyuebin@ygsoft.com>
2016-08-31 17:27:37 +0800

    code

---
+++ SaleManager/CommodityManager/CommodityAdd.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SaleManager
+{
+    partial class CommodityAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.价格 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(102, 94);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(102, 49);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 21);
+            this.txtCode.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(154, 192);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "编号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "名称";
+            // 
+            // 价格
+            // 
+            this.价格.AutoSize = true;
+            this.价格.Location = new System.Drawing.Point(38, 133);
+            this.价格.Name = "价格";
+            this.价格.Size = new System.Drawing.Size(29, 12);
+            this.价格.TabIndex = 14;
+            this.价格.Text = "名称";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(102, 133);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 21);
+            this.txtPrice.TabIndex = 13;
+            // 
+            // CommodityAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 271);
+            this.Controls.Add(this.价格);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnSave);
+            this.Name = "CommodityAdd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CommondityAdd";
+            this.Load += new System.EventHandler(this.CommodityAdd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label 价格;
+        private System.Windows.Forms.TextBox txtPrice;
+    }
+}
---
+++ SaleManager/CommodityManager/CommodityAdd.cs
@@ -0,0 +1,62 @@
+﻿using DataLayer;
+using Models;
+using Models.SaleEnum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleManager
+{
+    public partial class CommodityAdd : Form
+    {
+        public Guid Id { get; set; }
+        public CommodityAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            CommodityDL dl = new CommodityDL();
+            if (Id == null || Id == Guid.Empty)
+            {
+                Commodity model = new Commodity();
+                model.Code = txtCode.Text;
+                model.Name = txtName.Text;
+                model.Price = double.Parse(txtPrice.Text);
+                dl.Add(model);
+            }
+            else
+            {
+                var model = dl.GetCommodityById(Id);
+
+                model.Code = txtCode.Text;
+                model.Name = txtName.Text;
+                model.Price = double.Parse(txtPrice.Text);
+                dl.Update(model);
+            }
+
+            this.Close();
+        }
+
+        private void CommodityAdd_Load(object sender, EventArgs e)
+        {
+            if (Id != null && Id != Guid.Empty)
+            {
+                CommodityDL dl = new CommodityDL();
+                var model = dl.GetCommodityById(Id);
+
+                txtCode.Text = model.Code;
+                txtName.Text = model.Name;
+                txtPrice.Text = model.Price.ToString();
+            }
+
+        }
+    }
+}
